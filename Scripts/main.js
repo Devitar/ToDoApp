b26303ccff44681377f9126e221f3190923363ef
Devin Curtis <devin.curtis1210@gmail.com>
2019-02-21 10:04:05 -0700

    Clean up HTML
    Using only what I need, need to clean up CSS and take out unnecessary CDNs

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,11 @@
-$(function(){
-    console.log("Loaded.");
-});
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
 
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
