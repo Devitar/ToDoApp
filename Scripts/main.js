bd42ad56caa265201ffb184e5466e125818be228
Devin Curtis <devin.curtis1210@gmail.com>
2019-02-21 10:31:01 -0700

    Trimmed HTML, CDNs and SCSS

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
-
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar, #content').toggleClass('active');
         $('.collapse.in').toggleClass('in');
