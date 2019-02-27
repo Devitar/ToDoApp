5dcfd27b2e647e0a3cc423c5807d394cc99608e2
Devin Curtis <devin.curtis1210@gmail.com>
2019-02-27 10:33:46 -0700

    Added notes
    Future reference

--- Scripts/main.js
+++ Scripts/main.js
@@ -37,7 +37,8 @@ if (savedTasks != null){
     currentTasks = JSON.parse(savedTasks);
     allLists = Object.keys(currentTasks);
     for (let i = 0; i < allLists.length; i++){
-        //Add list to HTML
+        //Must loop through each, and parse it
+        //Add list to HTML with jQuery .append()
     };
 }else{
     //First time run, prompt user to create a new list
@@ -46,3 +47,40 @@ if (savedTasks != null){
 function SaveData(){
     localStorage.setItem("SavedTasks", JSON.stringify(currentTasks));
 };
+
+
+/* remember
+>jquery .children() returns an array of the element's children
+>use an upward counting variable to assign unique ID's to the lists
+>jquery .append() to add an HTML element to the prepended element
+
+
+sample code i made online as notes:
+
+let array1 = [
+  "<div class='task'></div>",
+  "<div class='task'></div>"
+];
+let array2 = [
+  "<div class='task2'></div>",
+  "<div class='task2'></div>"
+];
+
+let mainObj = {
+  Array1: array1,
+  Array2: array2
+};
+
+let stringedArray = JSON.stringify(mainObj);
+console.log(stringedArray);
+let parsedArray = JSON.parse(stringedArray);
+console.log(parsedArray);
+let allKeys = Object.keys(parsedArray);
+for (let i = 0; i < allKeys.length; i++){
+  let currentArray = parsedArray[allKeys[i]];
+  console.log(currentArray);
+  for (let i2 = 0; i2 < currentArray.length; i2++){
+    console.log(currentArray[i2]);
+  };
+};
+*/
