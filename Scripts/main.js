799787410510e3840f9507c4a17341237cd201da
Devitar <46533796+Devitar@users.noreply.github.com>
2019-03-07 17:36:14 -0700

    Overhaul #1
    JavaScript overhaul
    Using more OOP
    
    TODO: Save on change, Load on page load, reload tasks ( and add new IDs) on delete

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,4 @@
-let allLists = {}; //Main object, saves/loads to local storage
-let currentList = null; //Keeps track of the currently used list
-
 let savedTasks = localStorage.getItem("SavedTasks"); //Gets saved tasks, parsed later into allLists
-let currentId = localStorage.getItem("CurrentID");
 
 //Modal stuff
 let mainModal = $("#mainModal"); //Main greeting modal
@@ -13,7 +9,6 @@ let newListModalClose = $("#modalListClose");
 
 let newTaskModal = $("#createTaskModal");
 let newTaskModalClose = $("#modalTaskClose");
-
 //
 
 //FAB buttons
@@ -21,96 +16,6 @@ let mainFAB = $("#FABMain")
 let newListButton = $("#newList");
 let newTaskButton = $("#newTask");
 //
-
-let starOff = "Assets/Images/starEmpty.png";
-let starOn = "Assets/Images/starFull.png";
-let doneOff = "Assets/Images/checkBoxEmpty.png";
-let doneOn = "Assets/Images/checkBoxFull.png";
-
-class Task {
-    constructor(description, header, list) {
-        this.Description = description;
-        this.Header = header;
-        list.Content.push(this);
-    };
-    AddToDOM() {
-        let taskString = "<div class=taskItem col-12 data-isDone=false data-isImportant=false> \
-        <a class=`importantImageClick`> \
-            <img src=Assets/Images/starEmpty.png alt=Empty Star Icon class=importantImage> \
-        </a> \
-        <div class=taskHeader> \
-            " + this.Header + " \
-        </div> \
-        <a class=doneImageClick> \
-            <img src=Assets/Images/checkBoxEmpty.png alt=Empty Check Box Icon class=doneImage> \
-        </a> \
-        <div class=taskBody> \
-            " + this.Description + " \
-        </div> \
-        </div> \
-        ";
-        tasksSpot.append(taskString);
-    };
-    DeleteAnimate() {
-        //this.animate({parameters},speed,callback);
-        $(this).delete();
-    };
-    ImportantAnimate() {
-        //this.animate({parameters},speed,callback);
-    };
-};
-class List {
-    constructor(name, initialize) {
-        this.Name = name;
-        this.Id = GenerateId();
-        this.Content = [];
-        if (initialize != null) {
-            console.log("Creating new non initial list");
-            allLists[this.Name] = this;
-            this.Save(); //Save on new list create
-        };
-        this.AddToDOM(); //Add list to DOM automatically on create
-    };
-    Load() {
-        if (currentList /= null) {
-            currentList.Save();
-        };
-        ClearPage();
-        currentList = this;
-        for (let i = 0; i < this.Content.length; i++) {
-            tasksSpot.append(this.Content[i]);
-        };
-    };
-    Save() {
-        // $(`#${this.Id}`)
-        this.Content = [];
-        let allTasks = $("#body").children();
-        for (let i = 0; i < allTasks.length; i++) {
-            this.Content.push(JSON.stringify(allTasks[i]));
-        };
-        SaveData();
-    };
-    AddToDOM() {
-        let listString = "<li class=listItem id=" + this.Id + "> \
-        <a>" + this.Name + "</a> \
-        </li>";
-        sideBarList.append(listString);
-    };
-};
-
-function ClearPage() {
-    $(".taskItem").remove();
-};
-
-function SaveData() {
-    localStorage.setItem("SavedTasks", JSON.stringify(allLists));
-    localStorage.setItem("CurrentID", currentId.toString());
-};
-
-function GenerateId() {
-    currentId = currentId + 1;
-    return currentId;
-};
 
 //Close the greeting modal
 modalCloseButton.click(function () {
@@ -169,6 +74,14 @@ $(".doneImageClick").click(function (event) {
 });
 //
 
+//Delete Task Single
+$(".garbageImageClick").click(function (event) {
+    let imageContainer = $(event.target);
+    let mainTask = imageContainer.parent().parent();
+
+});
+//
+
 // List click
 $("#list1").on("click", "li", function (event) {
     let target = $(this);
@@ -190,7 +103,8 @@ $("#modalListSave").click(function (event) {
     if (newListName == "") {
         newListName = "Unnamed List";
     };
-    let newTempList = new List(newListName, true);
+    masterList.NewList(newListName);
+    newListModal.css("display", "none");
 });
 //
 
@@ -207,11 +121,11 @@ $("#modalTaskSave").click(function (event) {
         newTaskName = "Unnamed Task";
     };
     let newTaskBody = $("#newTaskInputBody").val();
-    if (newTaskName == "") {
-        newTaskName = "Unnamed Task";
+    if (newTaskBody == "") {
+        newTaskBody = "No task description.";
     };
-
-    // let newTempTask = new Task("", "", currentList);
+    currentList.NewTask(newTaskName, newTaskBody);
+    newTaskModal.css("display", "none");
 });
 //
 
@@ -222,24 +136,10 @@ $(document).ready(function () {
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
-    if (savedTasks != null) {
-        currentId = Number(currentId);
-        if (isNaN(currentId)) {
-            console.log("currentId is NaN! Setting to 0");
-            currentId = 0;
-        };
-
-        allLists = JSON.parse(savedTasks);
-        numOfLists = Object.keys(allLists);
-        for (let i = 0; i < numOfLists.length; i++) {
-            let savedList = allLists[numOfLists[i]].Content;
-            let initialList = new List(numOfLists[i]);
-            initialList.Id
-        };
-    } else {
-        //Prompt to create new list
-        currentId = 0; //Set the current task ID count to 0 (first time run)
+    if (savedTasks == null){
         mainModal.css("display", "block");
+    }else{
+        //Load saved data
     };
 });
 //
@@ -251,33 +151,4 @@ $(document).ready(function () {
 >jquery $("elementThing").parents("div") this grabs the nearest parent div
     and creates a jquery object out of it.
 >setTimeout(function(){ alert("Hello"); }, 3000); waits 3 seconds
-
-sample code i made online as notes:
-
-let array1 = [
-  "<div class='task'></div>",
-  "<div class='task'></div>"
-];
-let array2 = [
-  "<div class='task2'></div>",
-  "<div class='task2'></div>"
-];
-
-let mainObj = {
-  Array1: array1,
-  Array2: array2
-};
-
-let stringedArray = JSON.stringify(mainObj);
-console.log(stringedArray);
-let parsedArray = JSON.parse(stringedArray);
-console.log(parsedArray);
-let allKeys = Object.keys(parsedArray);
-for (let i = 0; i < allKeys.length; i++){
-  let currentArray = parsedArray[allKeys[i]];
-  console.log(currentArray);
-  for (let i2 = 0; i2 < currentArray.length; i2++){
-    console.log(currentArray[i2]);
-  };
-};
 */
--- Scripts/model.js
+++ Scripts/model.js
@@ -1,7 +1,12 @@
-let masterList = new MainList;
+let currentList;
 
-let sideBarList = $("#list1");
-let tasksSpot = $("#body");
+let listSpot = $("#list1");
+let taskSpot = $("#body");
+
+let starOff = "Assets/Images/starEmpty.png";
+let starOn = "Assets/Images/starFull.png";
+let doneOff = "Assets/Images/checkBoxEmpty.png";
+let doneOn = "Assets/Images/checkBoxFull.png";
 
 class MainList {
     constructor() {
@@ -10,8 +15,12 @@ class MainList {
     NewList(name) {
         let newList = new List(name, this.AllLists.length);
         this.AllLists.push(newList);
-        let listString = `<li class="listItem" id="${newList.ID}"><a href="#">${newList.Name}</a></li>`;
-        listSpot.Append(listString);
+        let listString = `<li class="listItem" id="${newList.ID}">` +
+            `<a href="#">${newList.Name}</a>` +
+            `</li>`;
+        listSpot.append(listString);
+        newList.HTML = $(`#${newList.ID}`);
+        currentList = newList;
     }
     DeleteList(id) {
         this.AllLists.splice(id, 1);
@@ -21,7 +30,7 @@ class MainList {
     }
     LoadList(id) {
         let selectedList = this.AllLists[id];
-
+        ClearPage();
     }
 }
 
@@ -30,22 +39,61 @@ class List {
         this.Name = name;
         this.ID = id;
         this.Tasks = [];
+        this.HTML = null;
     }
     NewTask(title, body, isImportant = false, isDone = false) {
-        let newTask = new Task(title, body, "Task"+this.Tasks.length, isImportant, isDone);
+        let newTask = new Task(title, body, "Task" + this.Tasks.length, isImportant, isDone);
         this.Tasks.push(newTask);
-        
+
+        let importantImg = ((bool) => {
+            switch (bool) {
+                case true:
+                    return starOn;
+                case false:
+                    return starOff;
+            }
+        })(newTask.IsImportant);
+        let doneImg = ((bool) => {
+            switch (bool) {
+                case true:
+                    return doneOn;
+                case false:
+                    return doneOff;
+            }
+        })(newTask.IsDone);
+
+        let taskString =
+            `<div class="taskItem col-12" id="${newTask.ID}" data-isDone="${newTask.IsDone}" data-isImportant="${newTask.IsImportant}">` +
+            `<a class="importantImageClick">` +
+            `<img src="${importantImg}" alt="Star Icon" class="importantImage">` +
+            `</a>` +
+            `<div class="taskHeader">` +
+            newTask.Title +
+            `</div>` +
+            `<a class="doneImageClick">` +
+            `<img src="${doneImg}"  alt="Check Box Icon" class="doneImage">` +
+            `</a>` +
+            `<a class="garbageImageClick" onclick="">` +
+            `<img src="Assets/Images/garbageColored.png" alt="Garbage Can Icon" class="garbageImage">` +
+            `</a>` +
+            `<div class="taskBody">` +
+            newTask.Body +
+            `</div>` +
+            `</div>`;
+        taskSpot.append(taskString);
+
+        newList.HTML = $(`#${newList.ID}`);
     }
-    DeleteDone(){
-        let allDone = $('[data-isDone="true"]').map(function() {
+    DeleteDone() {
+        let allDone = $('[data-isDone="true"]').map(function () {
             return this;
         }).get();
-        for (let i = 0; i < allDone.length; i++){
+        for (let i = 0; i < allDone.length; i++) {
             let stringID = allDone[i].ID;
             let taskID = Number(taskID.substring(4));
             this.Tasks.splice(taskID, 1);
 
-            $(allDone[i]).remove();
+            $(allDone[stringID]).remove();
         }
     }
 }
@@ -57,5 +105,25 @@ class Task {
         this.ID = id;
         this.IsImportant = isImportant;
         this.IsDone = isDone;
+        this.HTML = null;
     }
 }
+
+function UpdateLists() {
+
+}
+
+function UpdateTasks(list) {
+
+}
+
+function ClearPage() {
+    $(".taskItem").remove();
+};
+
+function SaveData() {
+    localStorage.setItem("SavedTasks", JSON.stringify(allLists));
+    localStorage.setItem("CurrentID", currentId.toString());
+};
+
+let masterList = new MainList;
